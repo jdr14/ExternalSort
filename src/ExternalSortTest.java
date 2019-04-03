4191e797695d9f9c48d1e7ca5a7908f728076cb7
Joey Rodgers <jdr14@vt.edu>
2019-04-03 13:59:02 -0400

    Added skeleton of test file as well as some minor changes to minheap.

---
+++ src/ExternalSortTest.java
@@ -0,0 +1,32 @@
+// import java.io.ByteArrayOutputStream;
+// import java.io.PrintStream;
+import java.util.*;
+
+import student.TestCase;
+
+/**
+ * 
+ * @author Jovany Cabrera jovanyc4
+ * @author Joseph Rodgers jdr14
+ * @version 1.0.0
+ *
+ */
+public class ExternalSortTest extends TestCase 
+{
+    
+    @Override
+    /**
+     * Runs before each test case.  Set up the any structures 
+     * to be used in all tests
+     */
+    public void setUp() 
+    {   
+        // Set up first using default constructors
+    }
+    
+	public void testMinHeap() 
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
